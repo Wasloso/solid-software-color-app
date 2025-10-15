ec13ebd2b50e9bba7de60065683932f6ca57bb9e
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2025-10-15 15:59:18 +0200

    feat: implement ColorsCubit for color state management

---
+++ lib/src/colorful_background/cubit/colors_state.dart
@@ -0,0 +1,21 @@
+part of 'colors_cubit.dart';
+
+/// State class for [ColorsCubit] that holds the current color.
+@immutable
+final class ColorsState extends Equatable {
+  /// The current [Color].
+  final Color color;
+  final int alpha;
+
+  /// Constructor for [ColorsState] that requires a [Color].
+  const ColorsState({required this.color, required this.alpha});
+
+  /// Creates a copy of the current state with an optional new [Color] and
+  /// [alpha].
+  ColorsState copyWith({Color? color, int? alpha}) {
+    return ColorsState(color: color ?? this.color, alpha: alpha ?? this.alpha);
+  }
+
+  @override
+  List<Object> get props => [color, alpha];
+}
